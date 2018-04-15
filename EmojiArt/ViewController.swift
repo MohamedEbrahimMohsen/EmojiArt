62f978c43d16a1238ec6f203db910a12a7bf11af
Mohamed Mohsen <Mohamed.Ebrahim.Mohsen@gmail.comgit config --global user.name MohamedEbrahimMohsen>
2018-04-15 21:31:29 +0200

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by Mohamed Mohsen on 4/15/18.
+//  Copyright © 2018 Mohamed Mohsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
